e5eb8abf4d584143a193b1b2595147b98234e6dd
Roy S <r.scheffers@gmail.com>
2017-04-17 13:19:50 -0400

    17th April update - 1 kata

--- sorting.js
+++ sorting.js
@@ -1,3 +1,21 @@
+---
+// Date: 17th April 2017
+// Source: https://www.codewars.com/kata/organise-duplicate-numbers-in-list/train/javascript
+// Given an array of numbers, your function should return an array of arrays, where each subarray contains all the duplicates of a particular number. Subarrays should be in the same order as the first occurence of the number they contain:
+
+// Solution:
+const group = arr => arr.reduce((groups, value) => {
+
+    var index = groups.findIndex((elem) => elem[0] === value);
+    if (index >= 0) {
+      groups[index].push(value);
+      return groups;
+    } else return [...groups, [value]];
+
+  }, []);
+}
+group([3, 2, 6, 2, 1, 3]);
+
 // DATE: 16th April 2017
 // Source: https://www.codewars.com/kata/56f399b59821793533000683
 
