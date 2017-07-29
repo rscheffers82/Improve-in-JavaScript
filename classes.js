240d4252ae57246991e55bcb6feb4cab7300497c
Roy Scheffers <r.scheffers@gmail.com>
2017-07-29 15:04:50 +0200

    extend classes

--- classes.js
+++ classes.js
@@ -14,6 +14,39 @@ class Person {
   }
 }
 
-let me = new Person('Roy Scheffers', 34);
+class Employee extends Person {
+  constructor(name, age, title) {
+    super(name, age);
+    // super calls the parent class constructor function and sets the initial values like name and age.
+    this.title = title;
+  }
+  getGreeting() {
+    if(this.title) {
+      return `Hi! I am ${this.name}. I work as a ${this.title}.`;
+    } else {
+      return super.getGreeting();
+    }
+  }
+  hasJob() {
+    return `Has a job: ${!!this.title}`;
+  }
+}
+
+class Programmer extends Person {
+  constructor(name, age, preferredLanguage = 'assembly') {
+    super(name, age);
+    this.preferredLanguage = preferredLanguage;
+  }
+  getGreeting() {
+    return `Hi! I am ${this.name}. I am a ${this.preferredLanguage} developer.`;
+  }
+}
+
+let dave = new Employee('Dave');
+console.log(dave.getGreeting());
+
+let john = new Programmer('John', 37, 'JavaScript');
+console.log(john.getGreeting());
+
+let me = new Employee('Roy Scheffers', 34, 'Front-end Web Developer');
 console.log(me.getGreeting());
-console.log(me.getPersonDescription());
