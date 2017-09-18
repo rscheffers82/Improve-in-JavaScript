bc4c8b69d71b52e0843f08d518b431c94ae6fc1c
Roy Scheffers <r.scheffers@gmail.com>
2017-09-18 18:10:39 +0200

    update currying script with another example

--- Functional Programming/4-currying.js
+++ Functional Programming/4-currying.js
@@ -1,3 +1,4 @@
+// helper function
 function curry(fn, arity = fn.length) {
   return (function nextCurried(prevArgs) {
     return function curried(nextArg){
@@ -14,6 +15,12 @@ function curry(fn, arity = fn.length) {
 
 const add = (x,y) => x+y;
 
-const result = [1, 2, 3, 4, 5].map( curry ( add)(3) );
+const result1 = [1, 2, 3, 4, 5].map( curry ( add)(3) );
+console.log('1st map function: ', result1);
 
-console.log(result);
+// advantage of currying above partial (function is first defined but value only later)
+const adder = curry (add);
+
+// later specify the value to add with
+const result2 = [1, 2, 3, 4, 5].map (adder(7) );
+console.log('2nd map function: ', result2);
